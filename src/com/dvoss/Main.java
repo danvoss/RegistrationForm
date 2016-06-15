fc9698ba140bbbe8a157b9aa884cda52e610bb61
Dan <danielandrewvoss@gmail.com>
2016-06-15 13:25:23 -0400

    Ad db methods

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -2,16 +2,52 @@ package com.dvoss;
 
 import org.h2.tools.Server;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void createTable(Connection conn) throws SQLException {
         Statement stmt = conn.createStatement();
         stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, username VARCHAR, address VARCHAR, email VARCHAR)");
+    }
+
+    public static void insertUser(Connection conn, User user) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES (NULL, ?, ?, ?)");
+        stmt.setString(1, user.username);
+        stmt.setString(2, user.address);
+        stmt.setString(3, user.email);
+        stmt.execute();
+    }
+
+    public static ArrayList<User> selectUsers(Connection conn) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users");
+        ResultSet results = stmt.executeQuery();
+        ArrayList<User> users = new ArrayList<>();
+        while (results.next()) {
+            Integer id = results.getInt("id");
+            String username = results.getString("username");
+            String address = results.getString("address");
+            String email = results.getString("email");
+            User user = new User(id, username, address, email);
+            users.add(user);
+        }
+        return users;
+    }
+
+    public static void updateUser(Connection conn, User user) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE users SET username = ?, address = ?, email = ? WHERE id = ?");
+        stmt.setString(1, user.username);
+        stmt.setString(2, user.address);
+        stmt.setString(3, user.email);
+        stmt.setInt(4, user.id);
+        stmt.execute();
+    }
+
+    public static void deleteUser(Connection conn, Integer id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM users WHERE id = ?");
+        stmt.setInt(1, id);
+        stmt.execute();
     }
 
     public static void main(String[] args) throws SQLException {
